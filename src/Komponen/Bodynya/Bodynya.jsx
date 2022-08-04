4b179d95cdce2648d57d8d96361efd77534c7de5
Rama Dhea Yudhistira <ramadheayudhistira@gmail.com>
2022-08-04 21:56:57 +0700

    in local oke

--- src/Komponen/Bodynya/Bodynya.jsx
+++ src/Komponen/Bodynya/Bodynya.jsx
@@ -10,71 +10,115 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
 
-import * as Validator from 'validatorjs';
+// import * as Validator from 'validatorjs';
 
-const Input = ({ label,type,name,onChange }) => {
-    return(
-        <div>
-            <label>{label}:</label>
-            <br />
-            <input type={type} name={name} onChange={e=>onChange(e.target.value)}/>
-            <br />
-        </div>
-    )
-}
 
-const ShowError = ({ error }) => {
-    return(
-        <ul style={{color: 'red',marginleft:'-20px'}}>
-            {
-                error.map((error,index)=><li key={index}>{error}</li>)
-            }
-
-        </ul>
-    )
-}
 
 export default class Bodynya extends React.Component {
     state = {
-        email: '',
-        password: '',
-        error: []
+        field:
+        {email: '',
+        password: ''},
+        
+        errors: {
+            email: '',
+            password: ''
+
+        }
     }
 
-    hanldeLogin = (e) => {
-        e.preventDefault();
-        const { email, password } = this.state;
+    formOnValidation = (name,value) => {
+        //if email !value return email harus di isi
         
-        let data = { email,password};
-          
-          let rules = {
-            email: 'required|email',
-            password: 'min:8|required'
-          };
+        if(!value){
+         return 'Mohon di isi !' 
+        }
+        if (name === 'email'){
+            if(!value.includes('@')){
+                return 'Email tidak valid !'
+            }
+        }if (name === 'password'){
+            if(value.length <= 5){
+                return 'Password harus lebih dari 5 karakter !'
+            }
+        }
+          return false
+ 
+ 
+ 
+     }
 
-        let validation = new Validator(data, rules);
-        validation.passes();
-        //jika validasi sesuai rules maka alert sukses dan kembali ke halaman login
-        if(validation.fails()){
-            alert('Login Sukses');
-            this.props.history.push('/');
-        }
-        //jika tidak sesuai rules maka alert gagal dan tampilkan error
-        else{
-            alert('Login Gagal');
-            this.setState({error:
-                [alert(...validation.errors.get('email')),
-                alert(...validation.errors.get('password'))]
+
+
+
+    formOnchange = (event) =>{
+        const {field,errors} = this.state
+        const {value,name} = event.target
+        const error = this.formOnValidation(name,value)
+        this.setState({
+            field: {...field,[name]:value},
+            errors: {...errors,[name]:error}
+
+        })
+        
+        
+
+    }
+
+
+    formOnSubmitValidator = () => {
+        const{field}= this.state
+        const names = Object.keys(field)
+        const errors = {}
+        names.forEach((name)=> {
+            const value = field[name]
+            const error = this.formOnValidation(name,value)
+            errors[name] = error
+            
+        })
+        return errors
+    }
+
+
+    formOnSubmit = (event) => {
+        event.preventDefault()
+        const validasi = this.formOnSubmitValidator()
+        const isError = Object.values(validasi).some((item)=> item !== false)
+
+        if(isError){
+            this.setState({
+                errors: validasi
             })
+
+            return
+
         }
-           }
+        
+        const {field} = this.state
+        console.log(validasi ,field)
+        //munculkan alert berhasil login
+        alert('Selamat!!! Login Anda Berhasil')
+
+
+
+    }
+    
+
+    
+
+    
+
+           
   render() {
+    // console.log('Render')
+    const {field,errors} = this.state
+    
     return (
       <div className="Base container-fluid p-5  text-white" style={{ backgroundColor: '#212529' }}>
         <div className="py-5 text-center p-5">
             <div className="p-5 ini">
                 <Container fluid  className="p-5 text-center">
-                    <Card className="cardini" style={{ width: '550px' ,height: '500px' } } border="info">
+                    <Card className="cardini" style={{ width: '550px' ,height: '700px' } } border="info">
                         <Card.Body>
                             <Card.Title>
                                 <img
@@ -87,21 +131,28 @@ export default class Bodynya extends React.Component {
                             <Card.Title className="mt-3 mb-5">
                                 <h3>Login Page</h3>
                             </Card.Title>
-                            <Form onSubmit={this.hanldeLogin}>
+                            <Form onSubmit={this.formOnSubmit}>
                                 <Form.Group className="mb-3" controlId="formBasicEmail "  >
                                     <Form.Label>Email address</Form.Label>
-                                    <Form.Control type="email" placeholder="Enter email" className="text-white mb-3" style={{ backgroundColor: '#ff000000' }} 
-                                        onChange={value=>this.setState({email: value})} />
+                                    <Form.Control name="email"  type="email" placeholder="Enter email" className="text-white mb-3" style={{ backgroundColor: '#ff000000' }}
+                                    value = {field.email}
+                                    onChange={this.formOnchange} 
+                                    />
+                                    <p className="logError">{errors.email}</p>
                                     
                                 </Form.Group>
 
                                 <Form.Group className="mb-3" controlId="formBasicPassword">
                                     <Form.Label>Password</Form.Label>
-                                    <Form.Control type="password" placeholder="Password" className="text-white mb-3" style={{ backgroundColor: '#ff000000' }} 
-                                        onChange={value=>this.setState({password: value})}/>
+                                    <Form.Control name="password"  type="password" placeholder="Password" className="text-white mb-3" style={{ backgroundColor: '#ff000000' }}
+                                    value = {field.password} 
+                                    onChange={this.formOnchange}
+                                    />
+                                    
+                                    <p className="logError">{errors.password}</p>
                                 </Form.Group>
                                 
-                                <Button variant="primary" type="submit" className="loginHandle">
+                                <Button variant="primary" type="submit" className="loginHandle mt-3">
                                     Login
                                 </Button>
                             </Form>
